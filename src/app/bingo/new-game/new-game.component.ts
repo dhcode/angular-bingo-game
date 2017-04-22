43e7e84b89e05c7500aad9c4369b9f8073877615
DominikH <dominik@my1a.de>
2017-04-22 23:41:46 +0200

    More words and some optimizations

--- src/app/bingo/new-game/new-game.component.ts
+++ src/app/bingo/new-game/new-game.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { GameService } from '../game.service';
 import { Router } from '@angular/router';
 
